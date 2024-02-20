664a7a5c82480284ae65de108b9d53ba3249cba9
cylim <cylim226@gmail.com>
2024-02-20 13:01:37 +0800

    add sidebar

--- src/components/home/VideoScrollList.tsx
+++ src/components/home/VideoScrollList.tsx
@@ -3,6 +3,13 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { VideoScrollListItem } from "./VideoScrollListItem";
 import { fakedata } from "@/utils/fakedata";
+import { Sidebar } from "@/components/layouts/Sidebar";
+
+const sidebar = [
+  {title: 'Home', url: 'home'},
+  {title: 'Popular', url: 'popular'},
+  {title: 'Trending', url: 'trending'},
+]
 
 const VideoScrollList = () => {
   const [videos, setVideos] = useState(fakedata);
@@ -20,9 +27,12 @@ const VideoScrollList = () => {
     width={280}
     height={280}
     priority
-  /> : <div className={'scroll-container flex flex-col w-[100%]'}> 
-     {videos.map(renderItem)}
-  </div>
+  /> : <div className="flex flex-row justify-between w-[100%]">
+      <Sidebar items={sidebar} fallback={'home'} />
+    <div className={'scroll-container flex flex-col w-[100%]'}> 
+      {videos.map(renderItem)}
+    </div>
+  </div> 
 };
 
 export default VideoScrollList;
---
+++ src/components/layouts/Sidebar.tsx
@@ -0,0 +1,18 @@
+import { Link } from "@nextui-org/react"
+import NextLink from "next/link"
+import { useSearchParams } from "next/navigation"
+
+const SidebarItem = ({ item, fallback }: { item: any, fallback: string }) =>{
+  const params = useSearchParams()
+  const isActive = (params.get('tab') || fallback) === item.url
+
+  return <Link as={NextLink} href={`?tab=${item.url}`} className={`text-lg font-semibold px-4 py-2  ${isActive ? 'bg-blue-500 text-white rounded-md': ''}`}>{item.title}</Link>
+}
+
+export const Sidebar =({items, fallback}: {items: any, fallback: string}) =>{
+  return <div className="w-1/4">
+    <div className="flex flex-col gap-3">
+      {items.map((i: any) => <SidebarItem item={i} key={i.title} fallback={fallback} />)}
+    </div>
+  </div>
+}
